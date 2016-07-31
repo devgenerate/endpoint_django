dc4b206ed7dc7dc8125cd88b0a7a69da15c7f7dd
David Higuita <david.higuita.75@gmail.com>
2016-07-31 01:33:06 -0500

    Full API working. Get all users or user by id. Using RestFramework

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url(r'^get_users', views.get_users, name="get_users"),
-    url(r'^get_user/(?P<id>[0-9]+$)', views.get_user_by_id, name="get_user_by_id"),
+    url(r'^$', views.GetUsers.as_view()),
+    url(r'^get_user/(?P<id>[0-9]+$)', views.GetUserById.as_view()),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,38 +1,18 @@
-from django.shortcuts import render
-from django.template import RequestContext
-from django.http import JsonResponse, HttpResponse
-from django.core import serializers
-import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from . import models
+from . import serializers
 
-# Create your views here.
 
-def home(request):
-    if request.method == "GET":
+class GetUsers(APIView):
+    def get(self, request, format=None):
         all_users = models.User.objects.all()
-        return render(request, "get_users.html")
+        serializer = serializers.UserSerializer(all_users, many=True)
+        return Response(serializer.data)
 
-def get_users(request):
-    print("NEW REQUEST")
-    print(request.method)
-
-    if request.method == "POST":
-        print(request.POST)
-        all_users = models.User.objects.all()
-        return JsonResponse(json.loads(serializers.serialize("json", all_users)), safe=False)
-
-    else:
-        return JsonResponse({"msg": "error"})
-
-
-def get_user_by_id(request, id):
-    if request.method == "GET":
-        print("GET USER BY ID")
-        print(id)
-        user_by_id = models.User.objects.filter(pk=id)
-        print(user_by_id)
-        return JsonResponse(json.loads(serializers.serialize("json", user_by_id)), safe=False)
-
-    return JsonResponse({"msg": "error"})
-
+class GetUserById(APIView):
+    def post(self, request, id, format=None):
+        user = models.User.objects.filter(pk=id)
+        serializer = serializers.UserSerializer(user, many=True)
+        return Response(serializer.data)
 
